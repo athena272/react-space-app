fb288a226d44ab490c82059a9b16da0937a03013
athena272 <guilhermera272@gmail.com>
2024-03-24 02:08:17 -0300

    Update NavigationItemStyles.jsx

--- src/components/NavigationItem/NavigationItemStyles.jsx
+++ src/components/NavigationItem/NavigationItemStyles.jsx
@@ -1,6 +1,10 @@
 import { styled } from "styled-components";
 
 const NavigationItemStyles = styled.li`
+    font-size: 1.5rem;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
 `
 
 export default NavigationItemStyles
