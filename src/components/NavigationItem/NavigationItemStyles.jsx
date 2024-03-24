4eb73869353d011180ba6f65d9321b543248427d
athena272 <guilhermera272@gmail.com>
2024-03-24 02:13:09 -0300

    Update NavigationItemStyles.jsx

--- src/components/NavigationItem/NavigationItemStyles.jsx
+++ src/components/NavigationItem/NavigationItemStyles.jsx
@@ -1,10 +1,14 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 const NavigationItemStyles = styled.li`
     font-size: 1.5rem;
     line-height: 29px;
     margin-bottom: 30px;
     cursor: pointer;
+    color: ${props => props.$isActive ? '#7B78E5' : '#D9D9D9'};
+    display: flex;
+    align-items: center;
+    gap: 22px;
 `
 
 export default NavigationItemStyles
