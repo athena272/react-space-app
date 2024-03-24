b3750a7f2371e4215e8510188ec991a577094e82
athena272 <guilhermera272@gmail.com>
2024-03-24 11:43:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { styled } from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Header from "./components/Header/Header"
 import AsideBar from "./components/AsideBar/AsideBar"
+import Banner from "./components/Banner/Banner"
+import bannerBackgroundImage from '/assets/images/banner.png'
 
 const BackgroundGradient = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -15,6 +17,10 @@ export default function App() {
       <GlobalStyles />
       <Header />
       <AsideBar />
+      <Banner
+        text={"A galeria mais completa de fotos do espaço!"}
+        backgroundImage={bannerBackgroundImage}
+      />
     </BackgroundGradient>
   )
 }
