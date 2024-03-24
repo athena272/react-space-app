fc316180f5a086b8448fff6e42304cafa7ccda2d
athena272 <guilhermera272@gmail.com>
2024-03-24 00:51:40 -0300

    Create AsideBar.jsx

---
+++ src/components/AsideBar/AsideBar.jsx
@@ -0,0 +1,15 @@
+export default function AsideBar() {
+    return (
+        <aside>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            Início
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
