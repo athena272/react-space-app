5da3809d4cf86b317ef6a5206dd3cc896e5ae11a
athena272 <guilhermera272@gmail.com>
2024-03-24 01:25:20 -0300

    Create AsideBarStyles.jsx

---
+++ src/components/AsideBar/AsideBarStyles.jsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const ListStyles = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`
+
+export default ListStyles
