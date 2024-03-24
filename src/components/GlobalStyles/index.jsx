beef84528a3d69e647e4e5ed3738b77300c25420
athena272 <guilhermera272@gmail.com>
2024-03-24 02:21:42 -0300

    Update index.jsx

--- src/components/GlobalStyles/index.jsx
+++ src/components/GlobalStyles/index.jsx
@@ -1,9 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
+import GandhiSansRegular from './fonts/GandhiSans-Regular.otf'
+import GandhiSansBold from './fonts/GandhiSans-Bold.otf'
 
 const GlobalStyles = createGlobalStyle`
 @font-face {
-  font-family: ;
-  src: url();
+  font-family: 'GandhiSansRegular';
+  src: local('Gandhi Sans Regular'), local('GandhiSansRegular'), url(${GandhiSansRegular});
+}
+
+@font-face {
+  font-family: 'GandhiSansBold';
+  src: local('Gandhi Sans Bold'), local('GandhiSansBold'), url(${GandhiSansBold});
 }
 
 html {
