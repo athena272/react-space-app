94773c38750c1fe059c79d054302a739a6afd4a4
athena272 <guilhermera272@gmail.com>
2024-03-24 00:47:58 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import HeaderStyles from './HeaderStyles'
+import TextField from '../TextField/TextField'
 
 export default function Header() {
     return (
         <HeaderStyles>
             <img src="assets/images/logo.png" alt="" />
+            <TextField />
         </HeaderStyles>
     )
 }
