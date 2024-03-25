acc2d0aa5fdf76c8090b3234d8bd31d4cf088e73
athena272 <guilhermera272@gmail.com>
2024-03-25 01:01:33 -0300

    Create AppStyes.jsx

---
+++ src/AppStyes.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const BackgroundGradient = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+export const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width:100%;
+`
+
+export const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
