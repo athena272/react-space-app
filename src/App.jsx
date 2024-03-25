b00c921f08b851a92641cdf6c318b1d70b5c317b
athena272 <guilhermera272@gmail.com>
2024-03-25 01:01:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { styled } from "styled-components"
+import { BackgroundGradient, AppContainer, MainContainer } from "./AppStyes"
 import GlobalStyles from "./components/GlobalStyles"
 import Header from "./components/Header/Header"
 import AsideBar from "./components/AsideBar/AsideBar"
 import Banner from "./components/Banner/Banner"
 import bannerBackgroundImage from '/assets/images/banner.png'
 
-const BackgroundGradient = styled.div`
-  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
-  width: 100%;
-  min-height: 100vh;
-`
-
 export default function App() {
   return (
     <BackgroundGradient>
       <GlobalStyles />
-      <Header />
-      <AsideBar />
-      <Banner
-        text={"A galeria mais completa de fotos do espaço!"}
-        backgroundImage={bannerBackgroundImage}
-      />
+      <AppContainer>
+        <Header />
+        <MainContainer>
+          <AsideBar />
+          <Banner
+            text={"A galeria mais completa de fotos do espaço!"}
+            backgroundImage={bannerBackgroundImage}
+          />
+        </MainContainer>
+      </AppContainer>
     </BackgroundGradient>
   )
 }
