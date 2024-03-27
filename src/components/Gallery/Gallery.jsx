7f1d8b5be021e2e8f81a5d6b75e634f1fe8703d6
athena272 <guilhermera272@gmail.com>
2024-03-26 23:17:17 -0300

    Update Gallery.jsx

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,8 +1,16 @@
 import Popular from '../Popular/Popular'
 import Tags from '../Tags/Tags'
 import { Title, GalleryContainer, FluidSection } from './GalleryStyles'
+import PropTypes from "prop-types"
 
-export default function Gallery() {
+Gallery.propTypes = {
+    photos: PropTypes.array,
+}
+
+export default function Gallery({ photos }) {
+    console.log("🚀 ~ Gallery ~ photos:", photos)
+    
+
     return (
         <>
             <Tags />
@@ -11,6 +19,13 @@ export default function Gallery() {
                     <Title>
                         Navegue pela galeria
                     </Title>
+                    {
+                        photos.map((photo, index) => (
+                            <li key={index}>
+                                {photo.title}
+                            </li>
+                        ))
+                    }
                 </FluidSection>
                 <Popular />
             </GalleryContainer>
