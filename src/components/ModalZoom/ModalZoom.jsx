9b23fa94088857dc6e23189b4f45091a481ebfe1
athena272 <guilhermera272@gmail.com>
2024-03-27 00:32:29 -0300

    Update ModalZoom.jsx

--- src/components/ModalZoom/ModalZoom.jsx
+++ src/components/ModalZoom/ModalZoom.jsx
@@ -1,12 +1,33 @@
 import PropTypes from "prop-types"
+import { Overlay, StyledDialog } from "./ModalZoomStyles"
+import { IconButton } from "../Gallery/Image/ImageStyles"
+import Image from "../Gallery/Image/Image"
 
 ModalZoom.propTypes = {
     photo: PropTypes.object,
+    onClose: PropTypes.func
 }
 
-export default function ModalZoom({ photo }) {
+export default function ModalZoom({ photo, onClose }) {
     return (
         <>
+            {
+                photo &&
+                <>
+                    <Overlay />
+                    <StyledDialog
+                        open={!!photo}
+                        onClose={onClose}
+                    >
+                        <Image photo={photo} expanded={true} />
+                        <form method="dialog">
+                            <IconButton formMethod="dialog">
+                                <img src="/assets/icons/fechar.png" alt="Icone de fechar" />
+                            </IconButton>
+                        </form>
+                    </StyledDialog>
+                </>
+            }
             <dialog open>
                 <p>Exemplo do Mozilla</p>
                 <form method="dialog">
