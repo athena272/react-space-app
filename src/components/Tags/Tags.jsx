807a15556c74e51e233e5498c21a0bacf425efa0
athena272 <guilhermera272@gmail.com>
2024-03-26 23:17:22 -0300

    Update Tags.jsx

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,7 +1,19 @@
 import tags from './tags.json'
+import { TitleTag, Tag } from './TagsStyles'
 
 export default function Tags() {
     return (
-        <p>Tags</p>
+        <>
+            <TitleTag>Busque por tags:</TitleTag>
+            {
+                tags.map((tag, index) => (
+                    <Tag
+                        key={index}
+                    >
+                        {tag.title}
+                    </Tag>
+                ))
+            }
+        </>
     )
 }
