4812b5727cb84f06ad968af761158f494d401922
athena272 <guilhermera272@gmail.com>
2024-03-27 00:16:50 -0300

    Update Popular.jsx

--- src/components/Gallery/Popular/Popular.jsx
+++ src/components/Gallery/Popular/Popular.jsx
@@ -1,7 +1,23 @@
 import { Title } from "../GalleryStyles";
+import { PhotosColumn, Image, Button } from "./PopularStyles";
+import popularsPhotos from './fotos-populares.json'
 
 export default function Popular() {
     return (
-        <Title $alignment="center">Populares</Title>
+        <section>
+            <Title $alignment="center">Populares</Title>
+            <PhotosColumn>
+                {
+                    popularsPhotos.map((photo, index) => (
+                        <Image
+                            key={index}
+                            src={photo.path}
+                            alt={photo.alt}
+                        />
+                    ))
+                }
+            </PhotosColumn>
+            <Button>Ver mais</Button>
+        </section>
     )
 }
