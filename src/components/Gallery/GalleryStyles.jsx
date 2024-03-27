4065eb5729649622341fb018343fe23ab8eab19c
athena272 <guilhermera272@gmail.com>
2024-03-26 22:45:16 -0300

    Update GalleryStyles.jsx

--- src/components/Gallery/GalleryStyles.jsx
+++ src/components/Gallery/GalleryStyles.jsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
-const Title = styled.h2`
+export const Title = styled.h2`
     color: #7B78E6;
     font-size: 32px;
     text-align: ${props => props.$alignment ? props.$alignment : 'left'};
 `
 
-export default Title
+export const GalleryContainer = styled.div`
+    display: flex;
+`
+
+export const FluidSection = styled.section`
+    flex-grow: 1;
+`
