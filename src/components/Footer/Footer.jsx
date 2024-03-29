df6258752af03c677e137ac7aa7fe0254c93e647
athena272 <guilhermera272@gmail.com>
2024-03-29 00:01:45 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,26 @@
+import { FooterContainer, IconsContainer, FooterText } from "./FooterStyles"
+
 export default function Footer() {
     return (
-        
+        <FooterContainer>
+            <IconsContainer>
+                <li>
+                    <a href="#">
+                        <img src="/assets/images/sociais/facebook.svg" alt="Icone Facebook" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/assets/images/sociais/instagram.svg" alt="Icone Instagram" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="/assets/images/sociais/twitter.svg" alt="Icone Twitter" />
+                    </a>
+                </li>
+            </IconsContainer>
+            <FooterText>Desenvolvido por Guilherme R. Alves</FooterText>
+        </FooterContainer>
     )
 }
