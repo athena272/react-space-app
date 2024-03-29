b370027805674c181315797e403d2323c02d793b
athena272 <guilhermera272@gmail.com>
2024-03-29 00:12:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,6 @@ export default function App() {
       <GlobalStyles />
       <AppContainer>
         <Header
-          filter={filter}
           setFilter={setFilter}
         />
         <MainContainer>
