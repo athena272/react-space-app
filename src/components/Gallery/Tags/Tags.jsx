cc844a091df17bbf30e86ff19fb0334b4317e964
athena272 <guilhermera272@gmail.com>
2024-03-29 00:05:53 -0300

    Update Tags.jsx

--- src/components/Gallery/Tags/Tags.jsx
+++ src/components/Gallery/Tags/Tags.jsx
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types"
 import tags from './tags.json'
 import { TagsContainer, TitleTag, Tag, Div } from './TagsStyles'
 
-export default function Tags() {
+Tags.propTypes = {
+    setTag: PropTypes.func,
+}
+
+export default function Tags({ setTag }) {
     return (
         <TagsContainer>
             <TitleTag>Busque por tags:</TitleTag>
@@ -10,6 +15,7 @@ export default function Tags() {
                     tags.map((tag, index) => (
                         <Tag
                             key={index}
+                            onClick={() => setTag(tag.tag)}
                         >
                             {tag.title}
                         </Tag>
