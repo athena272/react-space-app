b7cb2160ab20ffdb93538e0ebed6598139898960
athena272 <guilhermera272@gmail.com>
2024-03-28 23:57:15 -0300

    Update FooterStyles.jsx

--- src/components/Footer/FooterStyles.jsx
+++ src/components/Footer/FooterStyles.jsx
@@ -1,1 +1,12 @@
 import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-top: 100px;
+    background-color: #04244F;
+    padding: 22px;
+    box-sizing: border-box;
+`;
